ecfefeefca0958afab4170378f5e84e57ba3dd5f
Vivek Nair <vnair611@gmail.com>
2025-02-21 10:27:44 -0500

    chore: update package.json and refine prompts

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -18,18 +18,9 @@ const commitTypes: Record<CommitType, string> = {
 	 * Conventional Changelog:
 	 * https://github.com/conventional-changelog/conventional-changelog/blob/d0e5d5926c8addba74bc962553dd8bcfba90e228/packages/conventional-changelog-conventionalcommits/writer-opts.js#L182-L193
 	 */
-	conventional: `Choose a type from the type-to-description JSON below that best describes the git diff:\n${JSON.stringify(
+	conventional: `Choose exactly one type from the type-to-description JSON below that best describes the git diff. You must use one of these three types - no other types are allowed:\n${JSON.stringify(
 		{
-			docs: 'Documentation only changes',
-			style:
-				'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
-			refactor: 'A code change that neither fixes a bug nor adds a feature',
-			perf: 'A code change that improves performance',
-			test: 'Adding missing tests or correcting existing tests',
-			build: 'Changes that affect the build system or external dependencies',
-			ci: 'Changes to our CI configuration files and scripts',
 			chore: "Other changes that don't modify src or test files",
-			revert: 'Reverts a previous commit',
 			feat: 'A new feature',
 			fix: 'A bug fix',
 		},
